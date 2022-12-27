90b8f8ee7f4e0717668fe8bbf0951a16560a79cd
Evgeny Loy <evgenyloy@gmail.com>
2022-12-27 22:33:36 +0300

    hooks

--- src/components/countdown/CountDown.js
+++ src/components/countdown/CountDown.js
@@ -1,73 +1,75 @@
-import { Component } from 'react'
+import { useState } from 'react'
 
-class CountDown extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      days: '',
-      hours: '',
-      minutes: '',
-      seconds: '',
-    }
+function CountDown() {
+  const [days, setDays] = useState()
+  const [hours, setHours] = useState()
+  const [minutes, setMinutes] = useState()
+  const [seconds, setSeconds] = useState()
+
+  let interval
+
+  const startTimer = () => {
+    const year = new Date(`2023.01.01 00:00:00`)
+
+    interval = setInterval(() => {
+      const curentDate = new Date()
+      const difference = year - curentDate
+
+      const day = Math.floor(difference / 1000 / 60 / 60 / 24)
+      const hour = Math.floor(difference / 1000 / 60 / 60) % 24
+      const minute = Math.floor(difference / 1000 / 60) % 60
+      const second = Math.floor(difference / 1000) % 60
+
+      if (difference < 0) {
+        clearInterval(interval)
+      } else {
+        setDays(day)
+        setHours(hour)
+        setMinutes(minute)
+        setSeconds(second)
+      }
+    })
   }
 
-  countDownStart = () => {
-    const curentDate = new Date()
-    const year = new Date(`2024.01.01 00:00:00`)
-    const difference = year - curentDate
+  startTimer()
 
-    const day = Math.floor(difference / 1000 / 60 / 60 / 24)
-    const hour = Math.floor(difference / 1000 / 60 / 60) % 24
-    const minute = Math.floor(difference / 1000 / 60) % 60
-    const second = Math.floor(difference / 1000) % 60
-
-    this.setState({ days: day })
-    this.setState({ hours: hour })
-    this.setState({ minutes: minute })
-    this.setState({ seconds: second })
-  }
-
-  componentDidMount() {
-    this.countDownStart()
-    this.tmerId = setInterval(this.countDown)
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.tmerId)
-  }
-  render() {
-    const { days, hours, minutes, seconds } = this.state
-
-    return (
-      <>
-        <div className="main-title">New Year Countdown</div>
-        <div className="countdown">
-          <div className="countdown__block">
-            <div className="countdown__number-day number">{days}</div>
-            <div className="countdown__number-title">days</div>
-          </div>
-          <div className="countdown__block">
-            <div className="countdown__number-hour number">
-              {hours <= 9 ? '0' + hours : hours}
-            </div>
-            <div className="countdown__number-title">hours</div>
-          </div>
-          <div className="countdown__block">
-            <div className="countdown__number-minute number">
-              {minutes <= 9 ? '0' + minutes : minutes}
-            </div>
-            <div className="countdown__number-title">minutes</div>
-          </div>
-          <div className="countdown__block">
-            <div className="countdown__number-second number">
-              {seconds <= 9 ? '0' + seconds : seconds}
-            </div>
-            <div className="countdown__number-title">seconds</div>
+  return (
+    <>
+      <div className="main-title">New Year Countdown</div>
+      <div className="countdown">
+        <div className="countdown__block">
+          <div className="countdown__number-day number">{days}</div>
+          <div className="countdown__number-title">
+            {days < 2 ? 'day' : 'days'}
           </div>
         </div>
-      </>
-    )
-  }
+        <div className="countdown__block">
+          <div className="countdown__number-hour number">
+            {hours <= 9 ? '0' + hours : hours}
+          </div>
+          <div className="countdown__number-title">
+            {hours < 2 ? 'hour' : 'hours'}
+          </div>
+        </div>
+        <div className="countdown__block">
+          <div className="countdown__number-minute number">
+            {minutes <= 9 ? '0' + minutes : minutes}
+          </div>
+          <div className="countdown__number-title">
+            {minutes < 2 ? 'minute' : 'minutes'}
+          </div>
+        </div>
+        <div className="countdown__block">
+          <div className="countdown__number-second number">
+            {seconds <= 9 ? '0' + seconds : seconds}
+          </div>
+          <div className="countdown__number-title">
+            {seconds < 2 ? 'second' : 'seconds'}
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default CountDown
